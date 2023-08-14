b2f84160b735a891f07d045edf8826a571655d7a
Ariyan <98114776+ariyan-emi@users.noreply.github.com>
2023-08-14 19:12:33 +0330

    Update index.tsx
    Test

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ root.render(
         <React.StrictMode>
             <Provider store={store}>
                 <BrowserRouter>
-                    Hello
+                            <App/>
                 </BrowserRouter>
             </Provider>
         </React.StrictMode>
